f58a7b305a286fd627f51d4c7bf4cf701d8b3ff3
JoePizz <89032468+JoePizz@users.noreply.github.com>
2021-12-06 13:01:34 -0500

    Add updated R script

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,6 +11,18 @@ summary(lm(qsec ~ mpg + disp + drat + wt + hp, data = mtcars))
 
 Coil_Table <- read.csv(file='Suspension_Coil.csv')
 
-total_summary <- Coil_Table %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI),SD_PSI=sd(PSI))
+total_summary <- Coil_Table %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI),SD_PSI=sd(PSI)) #Summarize the PSI column with the measures of central tendency
 
-lot_summary <- Coil_Table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI),SD_PSI=sd(PSI), .groups = 'keep')
+lot_summary <- Coil_Table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI),SD_PSI=sd(PSI), .groups = 'keep') #Group the Manufacturing Lot column and summarize each group
+
+t.test(Coil_Table$PSI,mu= 1500)
+  
+filter_summary1 <- subset(Coil_Table, Manufacturing_Lot == 'Lot1')
+
+filter_summary2 <- subset(Coil_Table, Manufacturing_Lot == 'Lot2')
+
+filter_summary3 <- subset(Coil_Table, Manufacturing_Lot == 'Lot3')
+
+t.test(filter_summary1$PSI,mu=1500)
+t.test(filter_summary2$PSI,mu=1500)
+t.test(filter_summary3$PSI,mu=1500)
